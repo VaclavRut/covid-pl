beac263942262c8082ef5fc8db531c1efd9c0a80
Vaclavrut <vaclav@smartemailing.cz>
2020-03-18 09:14:52 +0100

    - fix for the whole country - added check for empty runs and sending email because of that

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ const latinize = require('latinize');
 
 const { log } = Apify.utils;
 const sourceUrl = 'https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2';
-const LATEST = "LATEST";
+const LATEST = 'LATEST';
+let check = false;
 
 Apify.main(async () => {
+    const { email } = await Apify.getValue('INPUT');
     const requestQueue = await Apify.openRequestQueue();
     const kvStore = await Apify.openKeyValueStore('COVID-19-POLAND');
-    const dataset = await Apify.openDataset("COVID-19-POLAND-HISTORY");
+    const dataset = await Apify.openDataset('COVID-19-POLAND-HISTORY');
 
     await requestQueue.addRequest({ url: sourceUrl });
     const crawler = new Apify.CheerioCrawler({
@@ -21,31 +23,37 @@ Apify.main(async () => {
             const now = new Date();
             const rawData = JSON.parse(latinize($('pre#registerData').text().trim()));
             const countryData = JSON.parse(rawData.parsedData);
-            log.info(`${countryData.length} of regions loaded.`)
+            log.info(`${countryData.length} of regions loaded.`);
             const infectedByRegion = [];
             let infectedCountTotal = 0;
             let deceasedCountTotal = 0;
+            if (countryData.length === 0) {
+                check = true;
+            }
             for (const region of countryData) {
                 const regionName = region.Wojewodztwo;
                 const city = region['Powiat/Miasto'];
                 const infectedCount = region.Liczba ? parseInt(region.Liczba) : 0;
                 const deceasedCount = region['Liczba zgonow'] ? parseInt(region['Liczba zgonow']) : 0;
-                infectedCountTotal += infectedCount;
-                deceasedCountTotal += deceasedCount;
 
-                infectedByRegion.push({
-                    region: regionName,
-                    city,
-                    infectedCount,
-                    deceasedCount,
-                });
+                if (regionName === 'Cala Polska') {
+                    infectedCountTotal = infectedCount;
+                    deceasedCountTotal = deceasedCount;
+                } else {
+                    infectedByRegion.push({
+                        region: regionName,
+                        city,
+                        infectedCount,
+                        deceasedCount,
+                    });
+                }
             }
             const data = {
                 infected: infectedCountTotal,
                 deceased: deceasedCountTotal,
                 infectedByRegion,
                 sourceUrl,
-                lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, now.getMinutes())).toISOString(),
+                lastUpdatedAtApify: new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes())).toISOString(),
                 readMe: 'https://apify.com/vaclavrut/covid-pl',
             };
 
@@ -55,13 +63,13 @@ Apify.main(async () => {
             const actual = Object.assign({}, data);
             delete actual.lastUpdatedAtApify;
 
-            if(JSON.stringify(latest)!== JSON.stringify(actual)){
-                log.info('Data did change :( storing new to dataset.')
+            if (JSON.stringify(latest) !== JSON.stringify(actual)) {
+                log.info('Data did change :( storing new to dataset.');
                 await dataset.pushData(data);
             }
 
             await kvStore.setValue(LATEST, data);
-            log.info('Data stored, finished.')
+            log.info('Data stored, finished.');
 
             // to have lovely public runs...
             await Apify.pushData(data);
@@ -77,4 +85,20 @@ Apify.main(async () => {
     await crawler.run();
 
     console.log('Crawler finished.');
+
+    // if there are no region data, send email, because that means something is wrong
+    const env = await Apify.getEnv();
+    if (check) {
+        await Apify.call(
+            'apify/send-mail',
+            {
+                to: email,
+                subject: `Covid-19 PL from ${env.startedAt} failed `,
+                html: `Hi, ${'<br/>'}
+                        <a href="https://my.apify.com/actors/${env.actorId}#/runs/${env.actorRunId}">this</a> 
+                        run had 0 regions, check it out.`,
+            },
+            { waitSecs: 0 },
+        );
+    }
 });
